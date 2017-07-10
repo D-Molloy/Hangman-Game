2f516cedc7e5705c98f386ff51a6ea97f938ec10
Denis Molloy <denis.molloy@gmail.com>
2017-07-10 12:16:10 -0400

    final updates...homework completegit add .

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Hangman-Game
+Star Wars: Stormtrooper Hangman
+
+Assignment #3 - due 7/10/17
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,14 @@
-var starWords = ["ackbar", "alderaan", "anakin", "bespin", "biggs", "C3PO", "calrissian", "chewbacca", "kashyyk", "coruscant", "dantooine", "dagobah", "destroyer", "droid", "ewok", "force", "galaxy", "geonosis", "greedo", "grievous", "imperial", "jedi", "kenobi", "kessel", "midichlorians", "organa", "podracing", "palpatine", "R2D2", "rebellion", "sandpeople", "skywalker", "solo", "stormtrooper", "tauntaun", "wedge", "windu", "wookie", "worshipfullness", "yavin"];
+var starWords = ["ackbar", "alderaan", "anakin", "bespin", "biggs", "c3po", "calrissian", "chewbacca", "kashyyk", "coruscant", "dantooine", "dagobah", "destroyer", "droid", "ewok", "force", "galaxy", "geonosis", "greedo", "grievous", "imperial", "jedi", "kenobi", "kessel", "midichlorians", "organa", "podracing", "palpatine", "r2d2", "rebellion", "sandpeople", "skywalker", "solo", "stormtrooper", "tauntaun", "wedge", "windu", "wookie", "worshipfullness", "yavin"];
 
 //generate a random number which will be the index for the arrayElement we will use for currentWord
-
 var random = Math.floor((Math.random()*(starWords.length - 1)));
+
 
 //select the currentWord from the Array
 var currentWord = starWords[random];
 
-// console.log(currentWord)
+//console.log(currentWord)
+
 //find the non-whitespace characters in currentWord and place them in an array
 var currentWordArray = currentWord.match(/\S/g);
 
@@ -28,7 +29,7 @@ printCurrentWord = function(){
 
 //ask the user if they want the full Star Wars Stormtrooper Hangman experience.  If True, play the song.
 strongForce = function() {
-	var userForceChoice = confirm("Do you want to feel the Force?");
+	var userForceChoice = confirm("Do you want to feel the Force flow through you?");
 	if (userForceChoice == true) {
 		var audioSource = document.querySelector("#audio-source");
 		audioSource.src="assets/sounds/star-wars-theme.mp3";
@@ -36,18 +37,52 @@ strongForce = function() {
 	}
 }
 
+
+//declare variables used in the onkeyup event
 var badGuessArray = [];
 var guessCount = 10;
 var badGuessCount = 0;
 var playerWins = 0;
-// var gameOver = false;
+var playerLosses = 0;
+
+// a function that resets some of the global variables
+globalOnKeyVarReset = function(){
+	badGuessArray = [];
+	guessCount = 10;
+	badGuessCount = 0;
+	
+	return badGuessArray;
+	return guessCount;
+	return badGuessCount;
+}
+
+
+// a function that resets the current word
+currentWordReset = function() {
+	random = Math.floor((Math.random()*(starWords.length - 1)));
+	currentWord = starWords[random];
+	currentWordArray = currentWord.match(/\S/g);
+	blanksArray = new Array(currentWordArray.length);
+	for (var i = 0; i < currentWordArray.length; i++) {
+		blanksArray[i] = "_ ";
+	}
+	return random;
+	return currentWord;
+	return currentWordArray;
+	return blanksArray
+}
+
+// a function called at the end of the game that resets some of the global variable, resets the current word, and prints the new blanksArray to the screen
+gameReset = function(){
+	globalOnKeyVarReset();
+	currentWordReset();
+	printCurrentWord();
+}
 
 // take the key the user presses and compare it to all of the characters in currentWord
 document.onkeyup = function(event) {
 	var userGuess = event.key;
 	var letterFound = false;
-	// var letterFound = false;
-	// var gameOver = false;
 
 	// console.log(userGuess);
 	// console.log(currentWordArray);
@@ -71,22 +106,17 @@ document.onkeyup = function(event) {
 		guessCount--;
 		document.querySelector(".remaining-guesses").innerHTML = guessCount;
 
-		// change the billboard, the current word, the image, and music to their losing state
+		// change the billboard, the image, the music to their losing state and resets the game
 		if (guessCount == 0) {
 			var billboard = document.querySelector(".billboard");
-			billboard.innerHTML="You lose!!!!";
-			billboard.style.fontSize = "80px";
-			billboard.style.backgroundColor = "black";
-			billboard.style.color = "red";	
+			billboard.innerHTML= "You lose! Press a letter to start a new round!";
 			document.querySelector(".hangman-img").src="assets/images/loser.gif";
-			var currentWord = document.querySelector(".current-word");
-			currentWord.innerHTML = currentWordArray.join(' ');
-			currentWord.style.color = "red";
-			currentWord.style.backgroundColor = "black";
 			var audioSource = document.querySelector("#audio-source");
 			audioSource.src="assets/sounds/march.mp3";
 			audioSource.play();
-			// gameOver = true;
+			playerLosses++;
+			document.querySelector(".player-losses").innerHTML = playerLosses;
+			gameReset();
 		}
 
 	}
@@ -97,30 +127,18 @@ document.onkeyup = function(event) {
 	var blanksArrayString = blanksArray.toString();
 	var currentWordArrayString = currentWordArray.toString();
 
-	//compare the two strings and if they are the same, change the billboard, image, music to their winning state
+	//compare the two strings and if they are the same, change the billboard, image, music to their winning state, and resets the game
 	if (blanksArrayString == currentWordArrayString) {
 		var billboard = document.querySelector(".billboard");
-		billboard.innerHTML="You win!!!!";
-		billboard.style.fontSize = "80px";
-		billboard.style.backgroundColor = "black";
-		billboard.style.color = "#7BEC54";	
+		billboard.innerHTML= "You win! Press a letter to start a new round!";
 		document.querySelector(".hangman-img").src="assets/images/dancing.gif";
 		var audioSource = document.querySelector("#audio-source");
 		audioSource.src="assets/sounds/cantina.mp3";
 		audioSource.play();
 		playerWins++;
 		document.querySelector(".player-wins").innerHTML = playerWins;
-		// gameOver = true;
+		gameReset();
 	}
-
-	// if (gameOver) {
-	// 	// var playNewGame = confirm("Play again?");
-	// 	if (playNewGame == true) {
-	// 		alert("Hey!");
-	// 	} else {
-
-	// 	}
-	// }
 }
 
 strongForce();
