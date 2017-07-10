7d4c65cccda83ec369e12e6eb1ac589ead3e4efe
Denis Molloy <denis.molloy@gmail.com>
2017-07-09 22:35:28 -0400

    Long overdue push

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-var starWords = ["alderaan", "anakin", "skywalker", "wookie", "chewbacca", "kashyyk", "coruscant", "dantooine", "dagobah", "destroyer", "droid", "ewoks", "force", "galaxy", "grievous", "solo", "calrissian", "windu", "midichlorians", "kenobi", "podracing", "palpatine", "sandpeople", "tautaun", "stormtrooper", "bespin", "geonosis", "yavin", "ackbar", "wedge"];
+var starWords = ["ackbar", "alderaan", "anakin", "bespin", "biggs", "C3PO", "calrissian", "chewbacca", "kashyyk", "coruscant", "dantooine", "dagobah", "destroyer", "droid", "ewok", "force", "galaxy", "geonosis", "greedo", "grievous", "imperial", "jedi", "kenobi", "kessel", "midichlorians", "podracing", "palpatine", "R2D2", "rebellion", "sandpeople", "skywalker", "solo", "stormtrooper", "tautaun", "wedge", "windu", "wookie", "worshipfullness", "yavin"];
 
 //generate a random number which will be the index for the arrayElement we will use for currentWord
 
@@ -7,22 +7,32 @@ var random = Math.floor((Math.random()*(starWords.length - 1)));
 //select the currentWord from the Array
 var currentWord = starWords[random];
 
+console.log(currentWord)
 //find the non-whitespace characters in currentWord and place them in an array
 var currentWordArray = currentWord.match(/\S/g);
+
 
 //create and array the same size as currentWordArray in which to place the blank spaces
 var blanksArray = new Array(currentWordArray.length);
 
-console.log(currentWord)
+
 
 //fill blanksArray with underscores representing all letters in currentWordArray
 for (var i = 0; i < currentWordArray.length; i++) {
 	blanksArray[i] = "_ ";
 }
 
-//remove the comma seperator from the blanks array using .join
-var noCommaBlanksArray = blanksArray.join('');
 
+//function to get a new word
+
+getNewWord = function() {
+	return currentWord = starWords[random];
+	return currentWordArray = currentWord.match(/\S/g);
+	return blanksArray = new Array(currentWordArray.length);
+}
+
+
+//ask the user if they want the full Star Wars Stormtrooper Hangman experience.  If True, play the song.
 strongForce = function() {
 	var userForceChoice = confirm("Do you want to feel the Force?");
 	if (userForceChoice == true) {
@@ -33,28 +43,96 @@ strongForce = function() {
 }
 
 
-//insert the current noCommaBlanksArray into the current-word tag
+//function that inserts the current blanksArray into the current-word tag
+
 printCurrentWord = function(){
 	var currentWordHTML = document.querySelector(".current-word");
-	currentWordHTML.innerHTML = noCommaBlanksArray;
+	currentWordHTML.innerHTML = blanksArray.join(' ');
 }
 
 // take the key the user presses and compare it to all of the characters in currentWord
 
+var badGuessArray = [];
+var guessCount = 6;
+var badGuessCount = 0;
+// var goodGuessCount = 0;
+var playerWins = 0;
+
 document.onkeyup = function(event) {
 	var userGuess = event.key;
-	var badGuessArray = [];
-	var badGuessCount = 0;
+	var letterFound = false;
+//create a wins variable
+	
 	console.log(userGuess);
+	// console.log(indexOfLetter);
 	console.log(currentWordArray);
 	for (var i = 0; i < currentWordArray.length; i++) {
-		if (currentWordArray[i] === userGuess){
-			console.log("correct");
-			noCommaBlanksArray[i] = userGuess;
-			console.log(noCommaBlanksArray);
+		if (userGuess == currentWordArray[i]) {
+			// console.log("correct");
+			blanksArray[i] = userGuess;
+			console.log(blanksArray);
+			document.querySelector(".current-word").innerHTML = blanksArray.join(' ');
+			letterFound = true;
 		} 
+
+		
 	}
+
+	if (letterFound == false) {
+		badGuessArray.push(userGuess);
+		badGuessCount++;
+		document.querySelector(".wrong-guesses").innerHTML = badGuessArray;
+		document.querySelector(".hangman-img").src="assets/images/hangman" + badGuessCount + ".png";
+		
+	}
+
+	guessCount--;
+	document.querySelector(".remaining-guesses").innerHTML = guessCount;
+
+
+
+
+	var blanksArrayString = blanksArray.toString();
+	var currentWordArrayString = currentWordArray.toString();
+	if (blanksArrayString == currentWordArrayString) {
+		document.querySelector(".billboard").innerHTML="You win!!!!";
+		document.querySelector(".hangman-img").src="assets/images/dancing.gif";
+		var audioSource = document.querySelector("#audio-source");
+		audioSource.src="assets/sounds/cantina.mp3";
+		audioSource.play();
+		playerWins++;
+		document.querySelector(".player-wins").innerHTML = playerWins;
+		// var playNewGame = confirm("Play again?");
+		// if (playNewGame == true) {
+		// 	getNewWord();
+		// } else {
+
+		// }
+	}
+
+	//change song to cantina
+	//reset arrays? variables?
+	//playerWins++;
+	//print playerWins to html player-wins
+
+
 }
+
+
+/*
+
+To do:
+function for printing badguessarray
+
+compare to currentWordsArray - badGuessArray to end game
+
+
+*/
+
+
+
+
+
 
 strongForce();
 printCurrentWord();
